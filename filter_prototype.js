b1ad47ee0e5474b3fd17fea881bb2e868c774f38
Van Huynh <27980695+vdhuynh@users.noreply.github.com>
2023-07-02 17:30:06 -0500

    Create filter_prototype.js

---
+++ filter_prototype.js
@@ -0,0 +1,9 @@
+Array.prototype.myFilter = function(callback) {
+  const newArray = [];
+  for (let i = 0; i < this.length; i++) {
+    if (callback(this[i], i, this)) {
+      newArray.push(this[i]);
+    }
+  }
+  return newArray;
+};
