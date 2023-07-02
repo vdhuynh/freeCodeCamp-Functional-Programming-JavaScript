65441966b2329d14e33b914b221deac4ef55d076
Van Huynh <27980695+vdhuynh@users.noreply.github.com>
2023-07-02 18:21:16 -0500

    Create return_sorted_array.js

---
+++ return_sorted_array.js
@@ -0,0 +1,11 @@
+const globalArray = [5, 6, 3, 2, 9];
+
+function nonMutatingSort(arr) {
+  // Only change code below this line
+  return [...arr].sort((a, b) => a - b);
+
+  // Only change code above this line
+}
+
+const sortedArray = nonMutatingSort(globalArray);
+console.log(sortedArray);
